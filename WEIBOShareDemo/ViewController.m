2c4c183475cf9dee8f4b14904ed94a4a034083dd
赚发2 <924375908@qq.com>
2016-07-06 14:46:57 +0800

    完成

--- WEIBOShareDemo/ViewController.m
+++ WEIBOShareDemo/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "WBSPopShareView.h"
 
 @interface ViewController ()
 
@@ -17,7 +18,18 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    
 }
+- (IBAction)showPopView:(id)sender {
+    WBSPopShareView *popShareView = [[WBSPopShareView alloc] initWithFrameWithFrame:self.view.frame images:@[@"tabbar_compose_idea",@"tabbar_compose_photo",@"tabbar_compose_headlines",@"tabbar_compose_lbs",@"tabbar_compose_review",@"tabbar_compose_more"] titles:@[@"文字",@"照片/视屏",@"头条文章",@"签到",@"直播",@"更多"]];
+    [self.view addSubview:popShareView];
+    
+    popShareView.handleShareBlock = ^ (UIButton *sender){
+        NSLog(@"%@",sender);
+    };
+}
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
